6ebe3797ea6e7a997ccd84c4c9597d3759f2f039
tomavic <hbasheer@live.com>
2018-10-08 16:40:45 +0300

    initial

---
+++ src/app/components/home/home.js
@@ -0,0 +1,23 @@
+/* global angular */
+import webpackAngular from '../../../assets/images/webpackAngular.png';
+import './home.scss';
+
+/**
+ * Home view controller class.
+ */
+class HomeController {
+  /**
+   * Creates an instance of home controller(HomeController)
+   * @param {Object} $timeout - Instance of $timeout service
+   */
+  constructor($timeout) {
+    $timeout(() => this.name= "Home View", 0);
+    this.webpackAngular = webpackAngular;
+  }
+}
+
+HomeController.$inject = ['$timeout'];
+
+export default angular.module("home-module", [])
+  .controller('homeController', HomeController)
+  .name;
