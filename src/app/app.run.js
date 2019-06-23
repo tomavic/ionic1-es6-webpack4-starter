9745bf6d8becf3b9c62975f9eb952e9eeb5d906b
tomavic <hbasheer@live.com>
2019-06-23 16:40:28 +0200

    .

--- src/app/app.run.js
+++ src/app/app.run.js
@@ -1,14 +1,22 @@
 /*@ngInject*/
 const AppRun = ($ionicPlatform) => {
-    $ionicPlatform.ready(() => {
-        // if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-        //   $cordovaKeyboard.hideAccessoryBar(true);
-        //   $cordovaKeyboard.disableScroll(true);
-        // }
-        // if (window.StatusBar) {
-        //     $cordovaStatusbar.style(1);
-        // }
-    });
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs).
+        // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
+        // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
+        // useful especially with forms, though we would prefer giving the user a little more room
+        // to interact with the app.
+        if (window.cordova && window.Keyboard) {
+          window.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+    
+        if (window.StatusBar) {
+          // Set the statusbar to use the default style, tweak this to
+          // remove the status bar on iOS or change it to use white instead of dark colors.
+          StatusBar.styleDefault();
+        }
+      });
 };
 
 export default AppRun;
